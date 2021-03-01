83fb1e73d8d65aeffa9b477d4bda139d4777b463
RaquelRuiz4 <raquel.ruiz.alu@iescampanillas.com>
2021-03-01 12:59:55 +0100

    Commit para el trabajo de ED.

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author RaquelR
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 * @version 1.0
+	 * @category trabajo de ED
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
